5895374cd181638b34a16f6a962f23ac8ad6a550
ommunde <munderam3@mgail.com>
2025-04-08 11:05:14 +0530

    first commit

---
+++ operations.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int add(int v1,int v2);
+int sub(int v1,int v2);
+int mul(int v1,int v2);
+int div(int v1,int v2);
+
+int main(){
+	int opt,v1,v2;
+	printf("1:add,2:sub,3:mul,4:div:");
+	scanf("%d",&opt);
+	printf("Enter the operands:")
+	scanf("%d%d",&v1,&v2);
+	
+	if(opt == 1){
+		add(v1,v2);
+	}
+
+	else if(opt == 2){
+		sub(v1,v2);
+	}
+
+	else if(opt == 3){
+		mul(v1,v2);
+	}
+
+	else if(opt == 4){
+		add(v1,v2);
+	}
+
+
+}
+
+
+
+int add(int v1,int v2){
+	return (v1+v2);
+}
+
+int sub(int v1,int v2){
+	return (v1-v2);
+}
+
+int mul(int v1,int v2){
+	return (v1*v2);
+}
+
+int div(int v1,int v2){
+	return (v1/v2);
+}
